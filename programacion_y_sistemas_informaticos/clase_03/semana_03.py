acc2d45dd000b577ccd02bfefea12765501d9c07
Federico <federicohernansanches@gmail.com>
2025-04-08 12:39:03 -0300

    programacion clase 03-ejercicio

---
+++ programacion_y_sistemas_informaticos/clase_03/semana_03.py
@@ -0,0 +1,41 @@
+import random
+
+def crear_album(figus_total):
+    return [0] * figus_total
+
+def album_incompleto(A):
+    return True if 0 in A else False
+
+def comprar_figu(figus_total):
+    return random.randint(0, figus_total-1)
+
+
+def cuantas_figus(figus_total):
+    album_nuevo = crear_album(figus_total)
+    compras = 0
+    
+    while album_incompleto(album_nuevo):
+        compras += 1
+        album_nuevo[comprar_figu(figus_total)] += 1
+
+    return compras
+
+def experimento_figus(n_repeticiones, figus_total):
+    res = []
+    for i in range(n_repeticiones):
+        res.append(cuantas_figus(figus_total))
+    return round(sum(res) / len(res))
+
+def comprar_paquete(figus_total, figus_paquete):
+    paquete = []
+    for i in range(figus_paquete): paquete.append(comprar_figu(figus_total))
+    return paquete
+
+
+
+n_repeticiones = 100
+figus_total = 860
+figus_paquete = 5
+
+print(experimento_figus(n_repeticiones, figus_total))
+print(comprar_paquete(figus_total, figus_paquete))
