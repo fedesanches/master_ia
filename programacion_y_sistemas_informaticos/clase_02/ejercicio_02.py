f6c8c50528d4de63d9db41c83f3cd36704497cab
Federico Lenovo <fsanches@lenovo.com>
2025-04-08 12:37:24 -0300

    programacion clase 03

--- programacion_y_sistemas_informaticos/clase_02/ejercicio_02.py
+++ programacion_y_sistemas_informaticos/clase_02/ejercicio_02.py
@@ -8,4 +8,16 @@ def collatz(x):
         c += 1
     return print(f"La cantidad de pasos son {c}")
 
+
+def collatz_recursivo(x):
+    if x == 1:
+        return 0
+    else:
+        if x % 2 == 0:
+            x = x / 2
+        else:
+            x = 3 * x + 1
+        return collatz_recursivo(x) + 1
+
+
 collatz(25)
---
+++ programacion_y_sistemas_informaticos/clase_03/clase.py
@@ -0,0 +1,15 @@
+import random
+
+prueba = random.random()
+print(prueba)
+prueba = random.random()
+print(prueba)
+prueba = random.random()
+print(prueba)
+
+
+prueba = random.randint(1,6)
+print(prueba)
+
+prueba = random.choice(seq)
+print(prueba)
